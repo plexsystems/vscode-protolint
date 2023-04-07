aca5e56f31bb43fb38f44f8e16da4b48552c0495
AlexCannonball <42497203+AlexCannonball@users.noreply.github.com>
2023-04-07 23:16:09 +0300

    remove unnecessary async

--- src/extension.ts
+++ src/extension.ts
@@ -14,7 +14,7 @@ export function activate(context: vscode.ExtensionContext) {
   subscribeToDocumentChanges(context, diagnosticCollection);
 }
 
-async function runLint() {
+function runLint() {
   let editor = vscode.window.activeTextEditor;
   if (!editor) {
     return;
